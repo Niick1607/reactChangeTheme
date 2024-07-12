dd3b146c88bd1ed1a86c08333551aac800d4feea
Niick_1607 <105996755+Niick1607@users.noreply.github.com>
2024-07-12 11:16:40 -0300

    commit

---
+++ src/components/ChangeTheme..jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const ThemedComponent = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <div style={{
+      background: theme === 'light' ? '#fff' : '#333',
+      color: theme === 'light' ? '#000' : '#fff',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <button onClick={toggleTheme}>
+        Change Theme: {theme === 'light' ? 'escuro' : 'claro'}
+      </button>
+    </div>
+  );
+};
+
+export default {
+    ThemeProvider,
+    ThemedComponent
+}
